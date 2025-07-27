28196ea5d6c7b5f845ce15d348bb518a9c9b9e64
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-27 21:34:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
-    //Write your code here
+	const rows = parseInt(prompt("Input number of rows"));
+	const cols = parseInt(prompt("Input number of columns"));
+
+	const table = document.createElement("table");
+	 table.border = "1";
+     table.style.borderCollapse = "collapse";
+     table.style.marginTop = "20px";
+	for(let i = 0; i< rows; i++){
+		const tr = document.createElement("tr");
+		for(let j = 0; j < cols ; j++){
+			const td = document.createElement("td");
+			td.textContent = `Row- ${i} Column- ${j}`;
+		    tr.appendChild(td);
+			td.style.padding = "8px";
+		}
+		table.appendChild(tr);
+	}
+	document.body.appendChild(table);
   
 }
